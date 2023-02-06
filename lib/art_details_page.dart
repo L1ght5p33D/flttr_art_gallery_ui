a0e9f63c211d86ef04b1c03de11e9ddf05914ad2
ls <ryanadrig@gmail.com>
2023-02-05 22:44:11 -0800

    fitted pageview

--- lib/art_details_page.dart
+++ lib/art_details_page.dart
@@ -13,10 +13,13 @@ class ArtDetailsPage extends StatefulWidget {
 
 class _ArtDetailsPageState extends State<ArtDetailsPage> {
 
-  int art_image_chosen_idx = 0;
-  PageController page_controller = PageController(viewportFraction: 0.8, keepPage: true);
+  // int art_image_chosen_idx = 0;
+  PageController page_controller = PageController();
+
   @override
   Widget build(BuildContext context) {
+
+
     return SafeArea(child:Scaffold(body:
     Container(width: ss.width,
               height: ss.height,
@@ -36,71 +39,55 @@ class _ArtDetailsPageState extends State<ArtDetailsPage> {
           IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.star_outline)))
       ],),
 
-        Container(
-          width:ss.width,
-          height: ss.width * 1.2,
-          child:
-    //   ListView.builder(
-    //   scrollDirection: Axis.horizontal,
-    // controller: _scroll_controller,
-    // itemCount: 3,
-    // itemBuilder: (context, art_img_idx){
-    //
-    //    return
-    //      GestureDetector(
-    //          onTap: (){
-    //            setState(() {
-    //              art_image_chosen_idx = art_img_idx;
-    //            });
-    //          },
-    //          child:Padding(padding: EdgeInsets.all(ss.width*.04),
-    //           child: Image.asset(widget.as_item["image_path"],
-    //                               width: ss.width*.96,
-    //                               height: ss.height* .69,
-    //                               fit: BoxFit.cover,
-    //           )));
-    //
-    //   })),
+
           SingleChildScrollView(
-            reverse: true,
-                  child: SizedBox(
-                    height: ss.width*.96,
-                    child: PageView.builder(
+              child: Container(
+                  // color:Colors.red,
+                  height: ss.width,
+                width: ss.width,
+                child:
+
+
+                PageView.builder(
+                      padEnds: false,
                       controller: page_controller,
-                      // itemCount: pages.length,
-                      itemBuilder: (_, index) {
+                      itemBuilder: (context, index) {
                         return
-                             GestureDetector(
-                                 onTap: (){
-                                   setState(() {
-                                     art_image_chosen_idx = index;
-                                   });
-                                 },
-                                 child:Padding(padding: EdgeInsets.all(ss.width*.04),
-                                  child: Image.asset(widget.as_item["image_path"],
-                                                      width: ss.width*.96,
-                                                      height: ss.width*.96,
-                                                      fit: BoxFit.cover,
-                                  )));
+                        SizedBox(
+                            width:ss.width,
+                            // color: Colors.green[100 * index],
+                            child:Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                    Padding(
+                      padding: EdgeInsets.only(left:ss.width*.03),
+                        child:FittedBox(
+                                    fit: BoxFit.contain,
+                                    child:ClipRRect(
+                        borderRadius: BorderRadius.circular(ss.width*.1),
+                        child: Image.asset(widget.as_item["image_path"],
+                                                      width: ss.width * .88,
+                                                      height: ss.width * .88,
+                                                      fit: BoxFit.fitWidth,
+                        ))))]));
                       },
-                    ),
-                  ))),
+                    )
+                )),
 
       SmoothPageIndicator(
           // activeIndex: art_image_chosen_idx,
           count:  3,
           controller: page_controller,
           axisDirection: Axis.horizontal,
-          effect:  SlideEffect(
-              spacing:  ss.width*.02,
-              radius:  4.0,
-              dotWidth:  5.0,
-              dotHeight:  5.0,
-              paintStyle:  PaintingStyle.stroke,
-              strokeWidth:  1.5,
-              dotColor:  Colors.transparent,
-              activeDotColor:  Colors.deepOrange
-          ),
+          effect: ExpandingDotsEffect(
+                radius:  4.0,
+                dotWidth:  5.0,
+                dotHeight:  5.0,
+                paintStyle:  PaintingStyle.stroke,
+                strokeWidth:  1.5,
+                dotColor:  g_accent_color,
+                activeDotColor:  Colors.deepOrange
+          )
         )
 
     ],))));
