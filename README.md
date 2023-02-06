df3ebb84f95d26129fbbef14ec773c4b537a98d8
ls <ryanadrig@gmail.com>
2023-02-06 15:48:40 -0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # art_store_ui
 
+Flutter art store UI based off dribbble design
+
+Features animated navigation bar, animated carousel and indicators, reactions stack widget, friendly light theme with creative asymmetrical material buttons 
--- lib/as_globals.dart
+++ lib/as_globals.dart
@@ -8,6 +8,17 @@ Color g_accent_color = Color(0Xffaaa7d9);
 
 Color g_icon_inactive_color = Colors.black54;
 
+Map user_profilepic_data = {
+  "user1": "assets/images/user1_pp.png",
+  "user2": "assets/images/user2_pp.png",
+  "user3": "assets/images/user3_pp.png",
+  "user4": "assets/images/user4_pp.png",
+  "user5": "assets/images/user5_pp.png",
+  "user6": "assets/images/user6_pp.png",
+  "user7": "assets/images/user7_pp.png",
+  "user8": "assets/images/user8_pp.png",
+};
+
 List<Map> art_store_items = [
   {
     "main_title": "Universe",
@@ -51,13 +62,3 @@ List<Map> art_store_items = [
   }
 ] ;
 
-Map user_profilepic_data = {
-  "user1": "assets/images/user1_pp.png",
-  "user2": "assets/images/user2_pp.png",
-  "user3": "assets/images/user3_pp.png",
-  "user4": "assets/images/user4_pp.png",
-  "user5": "assets/images/user5_pp.png",
-  "user6": "assets/images/user6_pp.png",
-  "user7": "assets/images/user7_pp.png",
-  "user8": "assets/images/user8_pp.png",
-};
